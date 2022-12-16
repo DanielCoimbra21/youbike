b7a4882936149ec87a64f2a50d19f84e4cf36fdc
Fabio <fabio.maiodealmeida@students.hevs.ch>
2022-12-16 14:04:33 +0100

    New buttons layout

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 // ignore: depend_on_referenced_packages
@@ -49,7 +50,6 @@ class _MapPageState extends State<MapPage> {
   late List<LatLng> latlen = <LatLng>[];
   bool isSaveVisible = false;
   late RouteShape rs;
-
 
   Future<RouteShape> fetchRouteShape() async {
     final response;
@@ -162,88 +162,82 @@ class _MapPageState extends State<MapPage> {
               ),
               Visibility(
                 visible: isCancelBtnVisible,
-                child: Container(
-                  alignment: Alignment.topRight,
-                  margin: const EdgeInsets.only(top: 100, right: 20),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      markers.clear();
-                      if (latlen.isNotEmpty) {
-                        latlen.clear();
-                      }
-                      setState(() {
-                        currentNumbMarker = 0;
-                        isCancelBtnVisible = false;
-                        isUndoBtnVisible = false;
-                        isValidateBtnVisible = false;
-                      });
-                    },
-                    child: const Icon(Icons.cancel),
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: isSaveVisible,
-                child: Container(
-                  alignment: Alignment.topRight,
-                  margin: const EdgeInsets.only(top: 240, right: 20),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      addRoute(rs,"test2", AuthController.instance.auth.currentUser?.uid);
-                    },
-                    child: const Icon(Icons.save_alt),
-                  ),
-                ),
-              ),
-
-              Visibility(
-                visible: isUndoBtnVisible,
-                child: Container(
-                  alignment: Alignment.topRight,
-                  margin: const EdgeInsets.only(top: 180, right: 20),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      if (currentNumbMarker > 0) {
-                        markers.removeLast();
-                        if (latlen.isNotEmpty) {
-                          latlen.clear();
-                        }
-                        setState(() {
-                          currentNumbMarker -= 1;
-                          if (currentNumbMarker == 0) {
-                            isUndoBtnVisible = false;
-                            isCancelBtnVisible = false;
+                child: FabCircularMenu(
+                  fabMargin: const EdgeInsets.only(bottom: 75, right: 40),
+                  animationDuration: const Duration(milliseconds: 500),
+                  ringDiameter: MediaQuery.of(context).size.width * 0.9,
+                  ringWidth: MediaQuery.of(context).size.width * 0.2,
+                  fabElevation: 9.0,
+                  ringColor: Colors.transparent,
+                  children: <Widget>[
+                    Visibility(
+                      visible: isCancelBtnVisible,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          markers.clear();
+                          if (latlen.isNotEmpty) {
+                            latlen.clear();
                           }
-                        });
-                      }
-                      if (currentNumbMarker == 1) {
-                        isValidateBtnVisible = false;
-                      }
-                    },
-                    child: const Icon(Icons.undo),
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: isValidateBtnVisible,
-                child: Container(
-                  alignment: Alignment.bottomRight,
-                  margin: const EdgeInsets.only(bottom: 20, right: 20),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      futureRouteShape = fetchRouteShape();
-                      futureRouteShape.then(
-                        (value) {
                           setState(() {
-                            poly = value.polyline;
-                            isLoaded = true;
-                            route();
+                            currentNumbMarker = 0;
+                            isCancelBtnVisible = false;
+                            isUndoBtnVisible = false;
+                            isValidateBtnVisible = false;
                           });
                         },
-                      );
-                    },
-                    child: const Icon(Icons.check),
-                  ),
+                        child: const Icon(Icons.cancel),
+                      ),
+                    ),
+                    Visibility(
+                      visible: isUndoBtnVisible,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          if (currentNumbMarker > 0) {
+                            markers.removeLast();
+                            if (latlen.isNotEmpty) {
+                              latlen.clear();
+                            }
+                            setState(() {
+                              currentNumbMarker -= 1;
+                              if (currentNumbMarker == 0) {
+                                isUndoBtnVisible = false;
+                                isCancelBtnVisible = false;
+                              }
+                            });
+                          }
+                          if (currentNumbMarker == 1) {
+                            isValidateBtnVisible = false;
+                          }
+                        },
+                        child: const Icon(Icons.undo),
+                      ),
+                    ),
+                    Visibility(
+                      visible: isValidateBtnVisible,
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          futureRouteShape = fetchRouteShape();
+                          futureRouteShape.then(
+                            (value) {
+                              setState(() {
+                                poly = value.polyline;
+                                isLoaded = true;
+                                route();
+                              });
+                            },
+                          );
+                        },
+                        child: const Icon(Icons.check),
+                      ),
+                    ),
+                    Visibility(
+                      visible: isValidateBtnVisible,
+                      child: FloatingActionButton(
+                        onPressed: () {},
+                        child: const Icon(Icons.save_alt),
+                      ),
+                    ),
+                  ],
                 ),
               )
             ],
@@ -289,7 +283,7 @@ class _MapPageState extends State<MapPage> {
   }
 }
 
-addRoute(RouteShape rs ,String name, String? id) {
+addRoute(RouteShape rs, String name, String? id) {
   DatabaseManager db = DatabaseManager();
   db.addRoad(rs: rs, name: name, id: id);
 }
