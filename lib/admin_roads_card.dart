4262d1a42477a8f2321211749724a74f359f4a49
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-12-22 15:44:25 +0100

    Design Cards

--- lib/admin_roads_card.dart
+++ lib/admin_roads_card.dart
@@ -29,103 +29,118 @@ class MyAdminRoads extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(10),
-        child: Card(
-            color: Colors.transparent,
-            elevation: 0,
-            child: Padding(
-          padding: const EdgeInsets.all(0.0),
-          child: Column(children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 0.0),
-                  child: Text(road.name,
-                      style: Theme.of(context).textTheme.headline6),
-                )
+      padding: const EdgeInsets.all(10),
+      child: Card(
+        
+        elevation: 0,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 202, 68, 113),
+                Color.fromARGB(255, 185, 105, 132),
+                Color.fromARGB(255, 114, 164, 204),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    "Distance: ${road.distance} metres | Duration: ${road.duration} minutes |",
-                    style: const TextStyle(
-                      fontSize: 12,
-                    )),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(road.name,
+                          style: Theme.of(context).textTheme.headline6),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      "Distance: ${road.distance} metres | Duration: ${road.duration} minutes |",
+                      style: const TextStyle(
+                        fontSize: 12,
+                      )),
 
-                // Text.rich(
-                //   WidgetSpan(
-                //       child: Icon(
-                //     Icons.favorite_border_outlined,
-                //     color: Colors.pink,
-                //   )),
-                // ),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Elevation: ${road.elvDeparture} metres - ",
-                    style: const TextStyle(
-                      fontSize: 12,
-                    )),
-                Text("${road.elvArrival} metres",
-                    style: const TextStyle(
-                      fontSize: 12,
-                    )),
-              ],
-            ),
-            Visibility(
-                                  child: Container(
-                                    alignment: Alignment.topRight,
-                                    margin: const EdgeInsets.only(
-                                        left: 20, right: 40),
-                                    child: TextButton(
-                                      style: flatButtonStyle,
-                                      onPressed: () {
-                                        var name = road.name;
-                                        RouteShape routeShape = RouteShape(
-                                            polyline: road.polyline,
-                                            elvDeparture: road.elvDeparture,
-                                            elvArrival: road.elvArrival,
-                                            duration: road.duration,
-                                            distance: road.distance,
-                                            transportMode: road.transportMode);
+                  // Text.rich(
+                  //   WidgetSpan(
+                  //       child: Icon(
+                  //     Icons.favorite_border_outlined,
+                  //     color: Colors.pink,
+                  //   )),
+                  // ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Elevation: ${road.elvDeparture} metres - ",
+                      style: const TextStyle(
+                        fontSize: 12,
+                      )),
+                  Text("${road.elvArrival} metres",
+                      style: const TextStyle(
+                        fontSize: 12,
+                      )),
+                ],
+              ),
+              Visibility(
+                child: Container(
+                  alignment: Alignment.topRight,
+                  margin: const EdgeInsets.only(left: 20, right: 40),
+                  child: TextButton(
+                    style: flatButtonStyle,
+                    onPressed: () {
+                      var name = road.name;
+                      RouteShape routeShape = RouteShape(
+                          polyline: road.polyline,
+                          elvDeparture: road.elvDeparture,
+                          elvArrival: road.elvArrival,
+                          duration: road.duration,
+                          distance: road.distance,
+                          transportMode: road.transportMode);
 
-                                        db.deleteMyRoad(road.id);
-                                        final snackBar = SnackBar(
-                                          content: Text(
-                                              'Deleted route ${road.name}'),
-                                          action: SnackBarAction(
-                                            label: 'Undo',
-                                            onPressed: () {
-                                              db.addRoad(
-                                                  rs: routeShape,
-                                                  name: name,
-                                                  id: AuthController.instance
-                                                      .auth.currentUser?.uid);
-                                            },
-                                          ),
-                                        );
+                      db.deleteMyRoad(road.id);
+                      final snackBar = SnackBar(
+                        content: Text('Deleted route ${road.name}'),
+                        action: SnackBarAction(
+                          label: 'Undo',
+                          onPressed: () {
+                            db.addRoad(
+                                rs: routeShape,
+                                name: name,
+                                id: AuthController
+                                    .instance.auth.currentUser?.uid);
+                          },
+                        ),
+                      );
 
-                                        // Find the ScaffoldMessenger in the widget tree
-                                        // and use it to show a SnackBar.
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(snackBar);
-                                      },
-                                      child: const Icon(Icons.delete),
-                                    ),
-                                  ),
-                                ),
-            Row(
-              children: [
-                const Expanded(
-                    child: Divider(
-                  thickness: 1,
-                ))
-              ],
-            )
-          ]),
-        )));
+                      // Find the ScaffoldMessenger in the widget tree
+                      // and use it to show a SnackBar.
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: const Icon(Icons.delete),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                      child: Divider(
+                    thickness: 1,
+                  ))
+                ],
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
